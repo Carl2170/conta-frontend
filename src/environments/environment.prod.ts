23c8694062e34fbc80fb1ed40fced3072759d58c
CVargas2170 <vargascarlos2170@gmail.com>
2025-07-22 00:44:46 -0400

    add api url env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.contabolivia.com/graphql',
   appName: 'ContaBolivia',
+    graphqlEndpoint: 'https://ms-contabilidad.onrender.com/graphql' // Cambia según tu backend
+
 };
